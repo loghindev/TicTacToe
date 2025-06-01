64dc25432dce55105a9053783e8ef90cd8032291
Alexandru Loghin <loghin.dev@gmail.com>
2025-06-01 16:54:32 +0300

    v1.1 - organized code

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const unmutedIcon = document.getElementById("unmutedIcon");
 const startBtn = document.getElementById("startBtn");
 const gameboard = document.getElementById("gameboard");
 const player1Name = document.querySelector("#scoreBoard #player1 .name");
-const player1Score = document.querySelector("#scoreBoard #player1 .scoreValue");
+const player1Score = document.querySelector("#scoreBoard #player1 .scorespanue");
+const player1Spinner = document.querySelector("#scoreBoard #player1 .spinner");
 const player2Name = document.querySelector("#scoreBoard #player2 .name");
-const player2Scoare = document.querySelector("#scoreBoard #player2 .scoreValue");
-const tieScore = document.querySelector("#scoreBoard #tie .scoreValue");
+const player2Scoare = document.querySelector("#scoreBoard #player2 .scorespanue");
+const player2Spinner = document.querySelector("#scoreBoard #player2 .spinner");
+const tieScore = document.querySelector("#scoreBoard #tie .scorespanue");
 const opponentComputer = document.querySelector("#scoreBoard #opponentOption #vsBot");
 const opponentFriend = document.querySelector("#scoreBoard #opponentOption #vsFriend");
 const score = {
@@ -32,24 +34,19 @@ function setPlayer2() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  generateGameboard();
+  generateGameboard(); // sets 'click' event listeners on each cell already
   // when page loads -> user vs computer (default);
   updateInterface(player1Sign, player2Sign, opponentType);
 
   startBtn.addEventListener("click", () => {
     startBtn.classList.add("hide-btn");
     startBtn.disabled = true;
-    pending = true;
-    // Check Here
-    awaitComputerMove(currPlayer, Math.floor(Math.random() * 800) + 500);
-  });
-  if (opponentType === "computer" && currPlayer === player2Sign) {
-    // console.log("Prima miscare computer automat");
-    pending = true;
-    if (startBtn.classList.contains("hide-btn")) {
-      awaitComputerMove(currPlayer, Math.floor(Math.random() * 1000) + 1000);
+    if (currPlayer === player2Sign && opponentType === "computer") {
+      console.log("Computer a mutat in 'DOMContentLoaded'");
+      pending = true;
+      awaitComputerMove(currPlayer, Math.floor(Math.random() * 800) + 800);
     }
-  }
+  });
 });
 
 function generateGameboard() {
@@ -65,17 +62,22 @@ function generateGameboard() {
 }
 function setCell(event) {
   if (event.target.textContent !== "" || pending || !startBtn.classList.contains("hide-btn")) return;
-  const val = document.createElement("span");
-  val.classList = "fade-in";
-  updatePlayerTurn(val);
-  event.target.appendChild(val);
-  // console.log(`CurrPlayer ${currPlayer}`);
+  // check the current player to know what type of cell to set
+
+  console.log("Eu am mutat in 'setCell()'");
+  const span = document.createElement("span");
+  span.classList = "fade-in";
+  span.textContent = currPlayer;
+  event.target.appendChild(span);
+  currPlayer === player1Sign ? makeSound(userMoveSound) : makeSound(computerMoveSound);
+  updateCurrentPlayer();
+  updateTurnColors();
+
   if (opponentType === "computer") {
+    console.log("Computer a mutat in 'setCell()'");
     pending = true;
-    awaitComputerMove(currPlayer, Math.floor(Math.random() * 1000) + 1000);
+    awaitComputerMove(currPlayer, Math.floor(Math.random() * 800) + 800);
   }
-  displayTurn();
-  makeSound(userMoveSound);
 }
 
 function awaitComputerMove(currPlayer, delay) {
@@ -85,14 +87,14 @@ function awaitComputerMove(currPlayer, delay) {
   console.log(emptyCells);
 
   if (emptyCells.length) {
-    const val = document.createElement("span");
+    const span = document.createElement("span");
     setTimeout(() => {
       pending = false;
-      val.classList = "fade-in";
-      val.textContent = currPlayer;
-      emptyCells[Math.floor(Math.random() * emptyCells.length)].appendChild(val);
-      updatePlayerTurn(val);
-      displayTurn();
+      span.classList = "fade-in";
+      span.textContent = currPlayer;
+      emptyCells[Math.floor(Math.random() * emptyCells.length)].appendChild(span);
+      updateCurrentPlayer();
+      updateTurnColors();
       makeSound(computerMoveSound);
     }, delay);
   }
@@ -107,9 +109,9 @@ function awaitComputerMove(currPlayer, delay) {
 function updateInterface(p1Sign, p2Sign, opponent) {
   player1Name.textContent = `Player 1 (${p1Sign})`;
   player2Name.textContent = `${opponent === "computer" ? "Computer " : "Player 2"} (${p2Sign})`;
-  displayTurn();
+  updateTurnColors();
 }
-function displayTurn() {
+function updateTurnColors() {
   if (currPlayer === player1Sign) {
     player1Name.style.color = "white";
     player2Name.style.color = "var(--light-grey)";
@@ -118,8 +120,7 @@ function displayTurn() {
     player1Name.style.color = "var(--light-grey)";
   }
 }
-function updatePlayerTurn(val) {
-  val.textContent = currPlayer;
+function updateCurrentPlayer() {
   // update the current player
   if (currPlayer === player1Sign) {
     currPlayer = player2Sign;
@@ -157,7 +158,6 @@ opponentFriend.addEventListener("click", (event) => {
 
 function makeSound(sound) {
   if (unmutedIcon.classList.contains("hidden")) return;
-  // console.log(sound);
   sound.currentTime = 0;
   sound.play();
 }
